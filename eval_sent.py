c3da9eb2e2dde60ed2317792404485126b922a5c
Vansil <silvandeboer@gmail.com>
2019-04-23 04:38:10 +0200

    eval bug fix

--- eval_sent.py
+++ eval_sent.py
@@ -30,6 +30,30 @@ def prepare(params, samples):
     '''
     TODO: Prepare the vocabulary and embedding
     '''
+    
+    return
+
+def batcher(params, batch):
+    # Retrieve parameters
+    net = params['model']
+    dataloader = params['dataloader']
+    
+    # Encode batch through model
+    batch = [' '.join(sent) if sent != [] else '.' for sent in batch]
+    batch = dataloader.prepare_sentences(batch)
+    embeddings = net.encode(batch).numpy()
+
+    return embeddings
+
+
+# Set params for SentEval
+params_senteval = {'task_path': PATH_TO_DATA, 'usepytorch': True, 'kfold': 10, 'batch_size': 512}
+params_senteval['classifier'] = {'nhid': 0, 'optim': 'adam', 'batch_size': 64,
+                                 'tenacity': 5, 'epoch_size': 4}
+# Set up logger
+logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.DEBUG)
+
+if __name__ == "__main__":
     # Obtain GloVe word embeddings
     print("Loading GloVe embedding from "+embedding_path)
     glove_emb = data_utils.EmbeddingGlove(embedding_path)
@@ -54,31 +78,8 @@ def prepare(params, samples):
     net.load_state_dict(state_dict)
     print("Network architecture:\n\t{}".format(str(net)))
 
-    params.dataloader = dataloader
-    params.model      = net
-    return
-
-def batcher(params, batch):
-    # Retrieve parameters
-    net = params.model
-    dataloader = params.dataloader
-    
-    # Encode batch through model
-    batch = [' '.join(sent) if sent != [] else '.' for sent in batch]
-    batch = dataloader.prepare_sentences(batch)
-    embeddings = net.encode(batch).numpy()
-
-    return embeddings
-
-
-# Set params for SentEval
-params_senteval = {'task_path': PATH_TO_DATA, 'usepytorch': True, 'kfold': 10, 'batch_size': 512}
-params_senteval['classifier'] = {'nhid': 0, 'optim': 'adam', 'batch_size': 64,
-                                 'tenacity': 5, 'epoch_size': 4}
-# Set up logger
-logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.DEBUG)
-
-if __name__ == "__main__":
+    params_senteval['dataloader'] = dataloader
+    params_senteval['model']      = net
 
     se = senteval.engine.SE(params_senteval, batcher, prepare)
     transfer_tasks = ['STS12', 'STS13', 'STS14', 'STS15', 'STS16',
